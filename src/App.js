80d0c934e73cb070f95d5704818079b524eb2b0f
souviks72 <souvikmandalvit2016@gmail.com>
2020-09-28 02:04:25 +0530

    deploying

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      count : 0
+    }
+    this.clickMinus = this.clickMinus.bind(this);
+    this.clickPlus = this.clickPlus.bind(this);
+  }
+
+  clickPlus(){
+    this.setState(prevState => {
+      return {count: prevState.count+1}
+    });
+  }
+
+  clickMinus(){
+    this.setState(prevState => {
+      return {count: prevState.count-1}
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h4>{this.state.count}</h4>
+        <button onClick={this.clickPlus}>+</button>
+        <button onClick={this.clickMinus}>-</button>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
